ae418c72eaf9e6504d02ce31ae7863cf4d4f71fc
Ellen Marie Dash <me@duckie.co>
2024-11-10 04:32:22 -0500

    get most of the logic in place. in theory i just need to implement view()

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,78 @@
+//use iced::widget::{Column, TextInput};
+//use iced::widget::{button, column, text, text_input};
+use iced::widget::text;
+use iced::Element;
 use serde::{Deserialize, Serialize};
 
 mod project;
+mod project_list;
 mod random;
 mod task;
 mod time_commitment;
 
-use project::Project;
+pub use project::Project;
+pub use project_list::ProjectList;
+pub use task::Task;
+pub use time_commitment::TimeCommitment;
 
-#[derive(Default, Debug, Deserialize, Serialize)]
-struct State {
-    pub projects: Vec<Project>,
-    current_project_idx: usize,
+#[derive(Debug)]
+enum Message {
+    ProjectAdd,
+    ProjectRemove(usize),
+    ProjectSetName(usize, String),
+    ProjectSetDesc(usize, String),
+    ProjectSetNote(usize, String),
+    TaskAdd,
+    TaskRemove(usize),
+    TaskSetName(usize, String),
+    TaskSetDesc(usize, String),
+    TaskSetNote(usize, String),
+    SwitchView(View),
 }
 
-impl State {
-    pub fn current_project(&self) -> Option<&Project> {
-        self.projects.get(self.current_project_idx)
-    }
-
-    pub fn current_project_mut(&mut self) -> Option<&mut Project> {
-        self.projects.get_mut(self.current_project_idx)
-    }
-
-    pub fn add_default_project(&mut self) {
-        self.projects.push(Project::default());
-    }
+#[derive(Debug, Default, Deserialize, Serialize)]
+enum View {
+    #[default]
+    Task,
+    Project,
+    ProjectList,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[derive(Default)]
+struct App {
+    projects: ProjectList,
+    view: View,
+}
+
+fn update_inner(app: &mut App, message: Message) -> Result<(), Box<dyn std::error::Error>> {
+    match message {
+        Message::ProjectAdd => app.projects.push_default(),
+        Message::ProjectRemove(idx) => app.projects.remove(idx),
+        Message::ProjectSetName(idx, name) => app.projects.set_name(idx, name)?,
+        Message::ProjectSetDesc(idx, desc) => app.projects.set_desc(idx, desc)?,
+        Message::ProjectSetNote(idx, note) => app.projects.set_note(idx, note)?,
+        Message::TaskAdd => app.projects.current_mut()?.push_default_task(),
+        Message::TaskRemove(idx) => app.projects.current_mut()?.remove_task(idx),
+        Message::TaskSetName(idx, name) => app.projects.current_mut()?.set_name(idx, name)?,
+        Message::TaskSetDesc(idx, desc) => app.projects.current_mut()?.set_desc(idx, desc)?,
+        Message::TaskSetNote(idx, note) => app.projects.current_mut()?.set_note(idx, note)?,
+        Message::SwitchView(view) => { app.view = view },
+    }
+
     Ok(())
+}
+
+fn update(app: &mut App, message: Message) {
+    match update_inner(app, message) {
+        Ok(()) => { },
+        Err(msg) => eprintln!("{}", msg),
+    }
+}
+
+fn view(_app: &App) -> Element<Message> {
+    text("hello").into()
+}
+
+fn main() -> iced::Result {
+    iced::run("STAYFOCUSED", update, view)
 }
--- src/project.rs
+++ src/project.rs
@@ -12,34 +12,48 @@ pub struct Project {
     pub note: String,
     pub time_commitment: TimeCommitment,
     tasks: Vec<Task>,
-    current_task_idx: usize,
+    idx: usize,
 }
 
 impl Project {
-    pub fn start_current_task(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        self.current_task_mut().ok_or("Project has no tasks")?.start();
+    pub fn iter(&self) -> std::slice::Iter<'_, Task> {
+        self.tasks.iter()
+    }
+
+    pub fn start_current_task(&mut self) -> Result<(), &str> {
+        self.current_task_mut()?.start();
         Ok(())
     }
 
-    pub fn stop_current_task(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        self.current_task_mut().ok_or("Project has no tasks")?.stop();
+    pub fn stop_current_task(&mut self) -> Result<(), &str> {
+        self.current_task_mut()?.stop();
         Ok(())
     }
 
-    pub fn current_task(&self) -> Option<&Task> {
-        self.tasks.get(self.current_task_idx)
+    pub fn current_task(&self) -> Result<&Task, &str> {
+        self.tasks.get(self.idx).ok_or("current task idx is invalid")
     }
 
-    pub fn current_task_mut(&mut self) -> Option<&mut Task> {
-        self.tasks.get_mut(self.current_task_idx)
+    pub fn current_task_mut(&mut self) -> Result<&mut Task, &str> {
+        self.tasks.get_mut(self.idx).ok_or("current task idx is invalid")
     }
 
     pub fn set_current_task(&mut self, idx: usize) {
-        self.current_task_idx = idx;
+        self.idx = idx;
     }
 
-    pub fn add_default_task(&mut self) {
+    pub fn push_default_task(&mut self) {
         self.tasks.push(Task::default());
+    }
+
+    pub fn remove_task(&mut self, idx_to_remove: usize) {
+        self.tasks.remove(idx_to_remove);
+
+        // The indexes for all values above idx_to_remove get shifted down one,
+        // so if self.idx > idx_to_remove, we need to account for that.
+        if self.idx > idx_to_remove {
+            self.idx -= 1;
+        }
     }
 
     pub fn has_time_commitment(&self) -> bool {
@@ -51,7 +65,22 @@ impl Project {
     }
 
     pub fn choose_random_task(&mut self) {
-        self.current_task_idx = random_different_index(&mut self.tasks, self.current_task_idx);
+        self.idx = random_different_index(&mut self.tasks, self.idx);
+    }
+
+    pub fn set_name(&mut self, idx: usize, name: String) -> Result<(), &str> {
+        self.tasks.get_mut(idx).ok_or("invalid index")?.name = name;
+        Ok(())
+    }
+
+    pub fn set_desc(&mut self, idx: usize, desc: String) -> Result<(), &str> {
+        self.tasks.get_mut(idx).ok_or("invalid index")?.description = desc;
+        Ok(())
+    }
+
+    pub fn set_note(&mut self, idx: usize, note: String) -> Result<(), &str> {
+        self.tasks.get_mut(idx).ok_or("invalid index")?.note = note;
+        Ok(())
     }
 }
 
@@ -70,7 +99,7 @@ mod tests {
         // there should be no current task yet.
         assert_eq!(project.current_task(), None);
 
-        project.add_default_task();
+        project.push_default_task();
         let task1 = project.tasks.last_mut().unwrap();
         task1.name = "task1 name".to_string();
         task1.description = "task1 description".to_string();
@@ -78,7 +107,7 @@ mod tests {
         // it should default to the first task.
         assert_eq!(project.current_task().unwrap().name, "task1 name");
 
-        project.add_default_task();
+        project.push_default_task();
         let task2 = project.tasks.last_mut().unwrap();
         task2.name = "task2 name".to_string();
         task2.description = "task2 description".to_string();
